f4f7f13e17ec580ea92eb171a9cafa8d59293bc6
Leann Fraoigh <d.myshlennikova@gmail.com>
2024-11-20 01:24:12 +0300

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>TODO list</h1>
+    </>
+  )
+}
+
+export default App
